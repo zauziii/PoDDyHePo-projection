57fb3aadd383a2569d07e257464ee884d6a7de91
zauziii <zhi.zhou@hotmail.com>
2022-05-01 00:33:29 +0300

    Updates
    updates on the package.

--- R/PoDDyHePoProjection.R
+++ R/PoDDyHePoProjection.R
@@ -4,11 +4,11 @@
 #'
 #' @description PoDDyHePoProjection is the main body for projection. It projects projects individual-level values of the variables using multiple imputation.
 #' 
-#' @details It allows people to set up a variable whose proportion of each category needs to be calculated and returns a \code{\link[=mids-class]{mids}} obeject.
+#' @details It allows people to set up a variable whose proportion of each category needs to be calculated and returns a \code{\link[=mids-class]{mids}} object.
 #' 
 #' @param data A well-formatted data set ready for projection.
 #' @param m Number of multiple imputations. The default is \code{m=5}.
-#' @param maxit A scalar giving the number of iterations. The default is 10.
+#' @param maxit A scalar giving the number of iterations. By default, `maxit = 10`.
 #' @param printFlag If `TRUE`, `mice` will print history on console. Use `print=FALSE` for silent computation.
 #' @param ... Formulas. Formulas.
 #'
